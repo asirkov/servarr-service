2cf94c0cc20c08256272effa8c3b3dc0dbb9dcff
asirkov <sanya.lyubchik99@gmail.com>
2025-07-15 23:31:22 +0300

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-sudo docker compose -f /docker/servarr/compose.yaml up -d transmission prowlarr sonarr radarr
-sudo docker compose -f /docker/jellyfin/compose.yaml up jellyfin jellyseerr -d
+sudo docker compose -f docker-compose.yaml up -d transmission prowlarr sonarr radarr
